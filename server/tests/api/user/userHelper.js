0a9338faef2803148ea0099e75691d6112a5e6ba
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-05 18:46:13 +0530

    add test cases for GET /api/user/ route

--- server/tests/api/user/userHelper.js
+++ server/tests/api/user/userHelper.js
@@ -1,8 +1,19 @@
 import request from 'supertest'
 import app from '../../../src/app.js'
 
+export const getToUser = (accessToken) =>
+  request(app)
+    .get('/api/user/')
+    .set({ authorization: `Bearer ${accessToken}` })
+
 export const postToForgotPassword = (dataToSend) =>
   request(app).post('/api/user/forgot-password').send(dataToSend)
 
 export const postToResetPassword = (dataToSend) =>
   request(app).post('/api/user/reset-password').send(dataToSend)
+
+export const getAccessToken = async (userData) => {
+  const res = await request(app).post('/api/auth/register').send(userData)
+
+  return res.body.accessToken
+}
