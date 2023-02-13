fe29469eff8cf2e636a3dd1fe98a245d65e3045a
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-13 21:25:55 +0530

    config redux store

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectTheme } from '../features/theme/themeSlice'
+
+function App() {
+  const theme = useSelector(selectTheme)
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+      localStorage.theme = 'dark'
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.theme = 'light'
+    }
+  }, [theme])
+
+  return <div className="App" />
+}
+
+export default App
