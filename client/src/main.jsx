fba5e50faa1f4342d0df5e871777de26266493e0
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-18 16:37:05 +0530

    add react-router-dom dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './features/store'
 import App from './components/App'
 import './styles/index.css'
@@ -8,7 +9,9 @@ import './styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
