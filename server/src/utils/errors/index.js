dba34ff7aa21446c840fb517abfa0a78e5783a8c
Mohit Ramani <75924391+mbramani@users.noreply.github.com>
2023-02-21 10:49:28 +0530

    main to 11-react-router-dom-setup (#27)
    * prettier fix
    
    * add user slice
    
    * add page slice
    
    * 11 react router dom setup to 19 navbar component (#20)
    
    * main to 11-react-router-dom-setup (#18)
    
    * prettier fix
    
    * add user slice
    
    * add page slice
    
    * update controllers
    
    * add react-router-dom dependencies
    
    * 11 react router dom setup to 19 navbar component (#20)
    
    * main to 11-react-router-dom-setup (#18)
    
    * prettier fix
    
    * add user slice
    
    * add page slice
    
    * update controllers
    
    * add react-router-dom dependencies
    
    * add Global styles
    
    * main to 19-navbar-component (#24)
    
    * 11 react router dom setup to 19 navbar component (#20)
    
    * main to 11-react-router-dom-setup (#18)
    
    * prettier fix
    
    * add user slice
    
    * add page slice
    
    * update controllers
    
    * add react-router-dom dependencies
    
    * add Global styles
    
    * add svg icons
    
    * add date utils
    
    * update global styles
    
    * add Navbar component
    
    * fix typo
    
    * fix lint-sataged
    
    * add vercel.json
    
    * Revert "add vercel.json"
    
    This reverts commit ec993c53ac27c5aa94973e1c479d7c427e2e1352.

--- server/src/utils/errors/index.js
+++ server/src/utils/errors/index.js
@@ -2,6 +2,6 @@ import BadRequestError from './bad-request.js'
 import CustomAPIError from './custom-api.js'
 import NotFoundError from './not-found.js'
 import UnauthenticatedError from './unauthenticated.js'
-import ForbiddenError from './Forbidden.js'
+import ForbiddenError from './forbidden.js'
 
 export { BadRequestError, CustomAPIError, NotFoundError, UnauthenticatedError, ForbiddenError }
