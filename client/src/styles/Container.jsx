7d9b875797abe80d2440cc8d8b627422ef644734
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-21 23:18:22 +0530

    add container and typography styles

---
+++ client/src/styles/Container.jsx
@@ -0,0 +1,5 @@
+import tw, { styled } from 'twin.macro'
+
+export const Container = styled.div`
+  ${tw`w-full max-w-2xl px-4 py-4 mx-auto my-4 h-fit`}
+`
---
+++ client/src/styles/TypographyStyles.jsx
@@ -0,0 +1,11 @@
+import tw, { styled } from 'twin.macro'
+
+export const PrimaryHeading = styled.h1`
+  ${tw`text-2xl font-bold`}
+`
+export const SecondaryHeading = styled.h2`
+  ${tw`text-xl font-bold`}
+`
+export const Paragraph = styled.p`
+  ${tw`text-base font-normal text-justify`}
+`
