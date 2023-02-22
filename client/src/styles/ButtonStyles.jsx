d226cc88d5379768d9b724d45e86e898a5e3f2e5
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-22 17:30:58 +0530

    add Button styles

---
+++ client/src/styles/ButtonStyles.jsx
@@ -0,0 +1,5 @@
+import tw, { styled } from 'twin.macro'
+
+export const PrimaryButton = styled.button`
+  ${tw`h-10 px-4 py-2 rounded-md shadow-xl w-fit shadow-gray-100 dark:shadow-gray-900 bg-gradient-to-b dark:from-gray-800/90 dark:to-slate-800/90 from-gray-200/90 to-slate-200/90 backdrop-filter backdrop-blur-lg hover:ring focus:ring ring-gray-800 dark:ring-gray-100 focus:outline-none ring-offset-2 dark:ring-offset-gray-900`}
+`
