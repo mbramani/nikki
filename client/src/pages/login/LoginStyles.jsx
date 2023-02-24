03ff31110ec5a5a9dcbc455aea62d4d29fae9566
Mohit Ramani <ramanimohit01@gmail.com>
2023-02-24 12:22:02 +0530

    update login page styles

--- client/src/pages/login/LoginStyles.jsx
+++ client/src/pages/login/LoginStyles.jsx
@@ -6,16 +6,16 @@ export const Container = styled(MainContainer)`
   ${tw`h-[75vh] flex justify-center items-center`}
 `
 export const FormContainer = styled.div`
-  ${tw`flex flex-col items-center justify-center w-full max-w-md border rounded-lg shadow-2xl dark:border-gray-800 h-96 shadow-gray-100 dark:shadow-gray-900 bg-gradient-to-t dark:from-gray-800/60 dark:to-slate-800/60 from-gray-200/60 to-slate-200/60 backdrop-filter backdrop-blur-lg`}
+  ${tw`flex flex-col items-center justify-center w-full h-full max-w-md border rounded-lg shadow-2xl dark:border-gray-800 shadow-gray-100 dark:shadow-gray-900 bg-gradient-to-t dark:from-gray-800/60 dark:to-slate-800/60 from-gray-200/60 to-slate-200/60 backdrop-filter backdrop-blur-lg`}
 
   button {
-    ${tw`w-full  h-12 mt-2 mb-1 text-lg font-medium bg-gradient-to-b dark:from-gray-800/80 dark:to-slate-800/80 from-gray-200/80 to-slate-200/80 hover:ring-2 focus:ring-2`}
+    ${tw`w-full h-12 mt-2 mb-1 text-lg font-medium bg-gradient-to-b dark:from-gray-800/80 dark:to-slate-800/80 from-gray-200/80 to-slate-200/80 hover:ring-2 focus:ring-2`}
   }
 `
 export const LoadingWrapper = styled.div`
-  ${tw`w-full flex flex-row items-center justify-center`}
+  ${tw`flex flex-row items-center justify-center w-full`}
   svg {
-    ${tw`animate-spin  -ml-4 mr-3 h-5 w-5`}
+    ${tw`w-5 h-5 mr-3 -ml-4 animate-spin`}
   }
   circle {
     ${tw`opacity-25`}
