a8eb5efe9f2b1f4b5019537ee892558dd29ce0e6
Mohit Ramani <ramanimohit01@gmail.com>
2023-03-02 17:41:36 +0530

    add skeletons styles

--- client/src/components/skeletons/index.jsx
+++ client/src/components/skeletons/index.jsx
@@ -1,4 +1,11 @@
+import tw, { styled } from 'twin.macro'
+import { Container } from '../../styles/Container'
 import { NavbarSkeleton } from '../navbar/NavbarStyles'
-import { TermSkeleton } from '../../pages/terms/TermsStyles'
 
-export { NavbarSkeleton, TermSkeleton }
+export const BlockSkeleton = styled(Container)`
+  ${tw`z-0 border border-gray-200 rounded-lg shadow h-[75vh] shadow-gray-100 dark:shadow-gray-900 bg-gradient-to-t dark:from-gray-800/60 dark:to-slate-800/60 from-gray-200/60 to-slate-200/60 backdrop-filter backdrop-blur-md dark:border-gray-800 animate-pulse`}
+`
+export const FromSkeleton = styled(BlockSkeleton)`
+  ${tw`max-w-md`}
+`
+export { NavbarSkeleton }
--- client/src/pages/terms/TermsStyles.jsx
+++ client/src/pages/terms/TermsStyles.jsx
@@ -1,5 +1,4 @@
 import tw, { styled } from 'twin.macro'
-import { Container } from '../../styles/Container'
 
 export const TermsSection = styled.section`
   h1 {
@@ -13,8 +12,4 @@ export const TermContainer = styled.div`
   p {
     ${tw`my-1`}
   }
-`
-
-export const TermSkeleton = styled(Container)`
-  ${tw`z-0 border border-gray-200 rounded-lg shadow h-[75vh] shadow-gray-100 dark:shadow-gray-900 bg-gradient-to-t dark:from-gray-800/60 dark:to-slate-800/60 from-gray-200/60 to-slate-200/60 backdrop-filter backdrop-blur-md dark:border-gray-800 animate-pulse`}
 `
