9970ffc8187e3a96cbf88879c6bbd615fe4844f5
Mohit Ramani <ramanimohit01@gmail.com>
2023-03-26 12:23:18 +0530

    fix PageHeader styles

--- client/src/routes/day/DayRouteStyles.jsx
+++ client/src/routes/day/DayRouteStyles.jsx
@@ -1,5 +1,5 @@
 import tw, { styled } from 'twin.macro'
-import { GradientDiv, SectionContainer } from '../RoutesStyles'
+import { GradientDiv } from '../RoutesStyles'
 
 export const PageContainer = styled(GradientDiv)`
   ${tw`[height: calc(100vh - 275px)] w-full  flex flex-col justify-center items-center`}
@@ -11,5 +11,5 @@ export const PageTextArea = styled.textarea`
   ${tw`[flex-grow: 0.85] relative rounded min-h-full w-full bg-transparent px-2 py-1 focus:ring-2 ring-gray-800 dark:ring-gray-100 focus:outline-none ring-offset-4 dark:ring-offset-gray-900`}
 `
 export const PageHeader = styled.div`
-  ${tw`w-full h-6 my-2`}
+  ${tw`w-full h-6 my-1 md:my-2 px-1`}
 `
